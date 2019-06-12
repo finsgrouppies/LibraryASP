9dcf0a7c218a7f51cd3639700bcf9dbd5ff37a67
finsgrouppies <finsgrouppies@gmail.com>
2019-06-12 11:18:28 +0300

    Dagi added the Book model class

---
+++ LibraryASP/Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace LibraryASP.Models
+{
+    public class Book
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        [StringLength(50, MinimumLength = 3)]
+        public string Title{ get; set; }
+        [StringLength(50, MinimumLength = 3)]
+
+        public string Author { get; set; }
+       
+
+        public Boolean Available { get; set; }
+        
+}
+}
