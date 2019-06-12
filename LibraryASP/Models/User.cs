7d79150be2dec42200669a18184c289e989ac61e
finsgrouppies <finsgrouppies@gmail.com>
2019-06-12 12:15:44 +0300

    Melo added user class

---
+++ LibraryASP/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibraryASP.Models
+{
+    public class User
+    {
+     
+        
+            public int ID { get; set; }
+
+            [StringLength(50, MinimumLength = 3)]
+            public string FName { get; set; }
+
+            [StringLength(50, MinimumLength = 3)]
+            public string LName { get; set; }
+
+
+        
+            public int  Bookborrowed { get; set; }
+
+            
+        }
+    }
